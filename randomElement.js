8d8a9631ff792f459efe40004d2a5100d4661aae
manpreet012 <84408163+manpreet012@users.noreply.github.com>
2021-07-10 12:14:24 +0530

    add randomElement2

--- randomElement.js
+++ randomElement.js
@@ -1,7 +1,11 @@
 var randomElement = ([1, 2, 3, 4])
 console.log(randomElement[2]);
 console.log(randomElement[0]);
- var randomElement1 = (["hi", true, [1, 2, 3, 4]])
- console.log(randomElement1[1]);
- console.log(randomElement1[2]);
- 
+
+var randomElement1 = (["hi", true, [1, 2, 3, 4]])
+console.log(randomElement1[1]);
+console.log(randomElement1[2]);
+
+var randomElement2 = (["how are you?", false,[9, 8, 7, 6, 5, 4]])
+console.log(randomElement2[0]);
+console.log(randomElement2[2]);
