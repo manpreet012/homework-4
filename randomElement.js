6e618dc58a3d992ccd2eb43d1ee40af54c004637
manpreet012 <84408163+manpreet012@users.noreply.github.com>
2021-07-10 11:58:37 +0530

    Add files via upload

---
+++ randomElement.js
@@ -0,0 +1,7 @@
+var randomElement = ([1, 2, 3, 4])
+console.log(randomElement[2]);
+console.log(randomElement[0]);
+ var randomElement1 = (["hi", true, [1, 2, 3, 4]])
+ console.log(randomElement1[1]);
+ console.log(randomElement1[2]);
+ 
